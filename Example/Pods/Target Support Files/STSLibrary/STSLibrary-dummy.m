d1ef45c1e77c81ca908950bc0151a37c253ab3ec
Smarttransactions <ray@smarttransactionsystems.com>
2017-01-27 22:46:44 +0530

    initial commit with pod structure.

---
+++ Example/Pods/Target Support Files/STSLibrary/STSLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_STSLibrary : NSObject
+@end
+@implementation PodsDummy_STSLibrary
+@end
---
+++ Example/Pods/Target Support Files/STSLibrary/STSLibrary-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double STSLibraryVersionNumber;
+FOUNDATION_EXPORT const unsigned char STSLibraryVersionString[];
+
