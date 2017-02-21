dcac12ee4aa863b5c90a589ff9b9da2a3fe2a806
ankitkumar2 <ankitk2@chetu.com>
2017-02-21 18:24:08 +0530

    updating pod version.

---
+++ Example/build/Pods.build/Debug-iphoneos/STSLibrary.build/DerivedSources/STSLibrary_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char STSLibraryVersionString[];
+ extern const double STSLibraryVersionNumber;
+
+ const unsigned char STSLibraryVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:STSLibrary  PROJECT:Pods-1" "\n";
+ const double STSLibraryVersionNumber __attribute__ ((used)) = (double)1.;
